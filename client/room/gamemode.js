ca68da1fc4182ab7a2d57f950e4e31dbd7416b1e
Majorhacker1 <116291812+Majorhacker1@users.noreply.github.com>
2024-07-12 00:46:36 +0300

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,29 +1,4 @@
-// Команды меню №1
-Damage.GetContext(). FriendlyFire.Value= GameMode. Parameters.GetBool ("Огонь По Своим");
-Damage.GetContext().DamageOut.Value = true
-BreackGraph.OnlyPlayerBlocksDmg = GameMode. Parameters.GetBool ("PartialDesruction");
-BreackGraph.WeakBlocks GameMode. Parameters.GetBool ("LoosenBlocks");
-Build.GetContext().FloodFill.Value = GameMode.Parameters.GetBool ("FloodFill");
-Build.GetContext(). FillQuad.Value = GameMode .Parameters.GetBool ("FillQuad");
-Build.GetContext(). RemoveQuad.Value = GameMode. Parameters.GetBool ("RemoveQuad");
-Build.GetContext(). FlyEnable.Value = GameMode.Parameters.GetBool ("Fly");
-// Разрушаемость
-BreackGraph.BreackAll = true;
-// Счётчик прямоугольников
-Ui.GetContext(). QuadsCount.Value = false;
-// Интерфейс и функционал
-Build.GetContext(). Pipette.Value = true;
-Build.GetContext(). BalkLenChange.Value = false;
-Build.GetContext().SetSkyEnable.Value = false;
-Build.GetContext().GenMapEnable.Value = false;
-Build.GetContext().ChangeCamera PointsEnable. Value = false;
-Build.GetContext(). QuadChangeEnable.Value = false;
-Build.GetContext(). BuildModeEnable.Value = false;
-Build.GetContext().CollapseChangeEnable. Value = false;
-Build.GetContext(). RenameMapEnable.Value = false;
-Build.GetContext().ChangeMapAuthorsEnable. Value = false;
-Build.GetContext(). LoadMapEnable.Value = true;
-Build.GetContext().ChangeSpawnsEnable.Value = false;
+l
 // Константа
 Properties.GetContext(). GameModeName.Value = "GameModes/Mytishchi RP";
 // Команды
@@ -78,18 +53,3 @@ Teams.OnPlayerChangeTeam.Add(function(player ){ player. Spawns.Spawn()});
 // Приветствующая таблица
 
 Ui.getContext().Hint.Value = "Добро пожаловать в Мытищи РП";
-
-// Инвентарь
-
-var inventory = Inventory.GetContext();
-inventory.MainInfinity.Value = true;
-inventory.SecondaryInfinity.Value = true;
-inventory.Melee.Value = true;
-inventory.Explosive.Value = true;
-inventory.Build.Value = true:
-
-// Функционал №3
-Build.GetContext().BlocksSet.Value = BuildBlocksSet.AllClear;
-
-// Время возраждения
-Spawns.GetContext(). RespawnTime.Value = 5;
