cc13b82640b5b25d24d1c46dbdf925ae50c93fca
Majorhacker1 <116291812+Majorhacker1@users.noreply.github.com>
2024-07-12 01:38:45 +0300

    Update options.js

--- client/room/options.js
+++ client/room/options.js
@@ -1,45 +1,91 @@
 import * as room from 'pixel_combats/room';
 import * as teams from './default_teams.js';
 
-// разрешаем все что можно для РП
-export function set_editor_inventory() {
-    var roomInventory = Inventory.GetContext();
-    roomInventory.MainInfinity.Value = true;
-    roomInventory.SecondaryInfinity.Value = true;
-    roomInventory.Melee.Value = true;
-    roomInventory.Explosive.Value = true;
-    roomInventory.Build.Value = true;
-    roomInventory.BuildInfinity.Value = true;
+// разрешает все что можно для строительства
+function set_inventory() {
+    const context = room.Inventory.GetContext();
+    context.Main.Value = false;
+    context.Secondary.Value = false;
+    context.Melee.Value = true;
+    context.Explosive.Value = false;
+    context.Build.Value = true;
+    context.BuildInfinity.Value = true;
 }
-    
-    if(GameMode.Parameters.GetBool("ГНИО")){
 
-    var roomInventory = Inventory.GetContext();
-    Teams.Get("Blue").Inventory.Main.Value = false;
-    Teams.Get("Blue").Inventory.Secondary.Value = false;
-    Teams.Get("Blue").Inventory.Melee.Value = false;
-    Teams.Get("Blue").Inventory.Explosive.Value = false;
-    Teams.Get("Blue").Inventory.Build.Value = false;
+function set_build_settings() {
+    const context = room.Build.GetContext();
+    // прочие опции
+    context.Pipette.Value = true;
+    context.BalkLenChange.Value = false;
+    context.SetSkyEnable.Value = false;
+    context.GenMapEnable.Value = false;
+    context.ChangeCameraPointsEnable.Value = false;
+    context.QuadChangeEnable.Value = false;
+    context.BuildModeEnable.Value = false;
+    context.CollapseChangeEnable.Value = false;
+    context.RenameMapEnable.Value = false;
+    context.ChangeMapAuthorsEnable.Value = false;
+    context.LoadMapEnable.Value = true;
+    context.ChangeSpawnsEnable.Value = false;
+    context.BlocksSet.Value = room.BuildBlocksSet.AllClear; // делаем возможность строительства всеми блоками
+}
 
+// задает в контекст инвентаря пустой инвентарь
+function set_empty_inventory(inventory) {
+    inventory.Main.Value = false;
+    inventory.Secondary.Value = false;
+    inventory.Melee.Value = false;
+    inventory.Explosive.Value = false;
+    inventory.Build.Value = false;
+}
 
-export function set_RP_options() {
-    Damage.GetContext().FriendlyFire.Value = GameMode.Parameters.GetBool("Огонь По Своим");
-    Damage.GetContext().DamageOut.Value = true
-    Build.GetContext().Pipette.Value = true;
-    Build.GetContext().FloodFill.Value = false;
-    Build.GetContext().FillQuad.Value = false;
-    Build.GetContext().RemoveQuad.Value = false;
-    Build.GetContext().BalkLenChange.Value = false;
-    Build.GetContext().FlyEnable.Value = false;
-    Build.GetContext().SetSkyEnable.Value = false;
-    Build.GetContext().GenMapEnable.Value = false;
-    Build.GetContext().ChangeCameraPointsEnable.Value = false;
-    Build.GetContext().QuadChangeEnable.Value = false;
-    Build.GetContext().BuildModeEnable.Value = false;
-    Build.GetContext().CollapseChangeEnable.Value = false;
-    Build.GetContext().RenameMapEnable.Value = false;
-    Build.GetContext().ChangeMapAuthorsEnable.Value = false;
-    Build.GetContext().LoadMapEnable.Value = false;
-    Build.GetContext().ChangeSpawnsEnable.Value = false;
-    Build.GetContext().BuildRangeEnable.Value = false;
+// задает опции режима мир, выбранные при создании комнаты
+export function apply_room_options() {
+    const gameModeParameters = room.GameMode.Parameters;
+
+    // опции строительства
+    const buildContext = room.Build.GetContext();
+    buildContext.FloodFill.Value = gameModeParameters.GetBool("FloodFill");
+    buildContext.FillQuad.Value = gameModeParameters.GetBool("FillQuad");
+    buildContext.RemoveQuad.Value = gameModeParameters.GetBool("RemoveQuad");
+    buildContext.FlyEnable.Value = gameModeParameters.GetBool("Fly");
+
+    // прочие опции
+    room.Damage.GetContext().FriendlyFire.Value = GameMode.Parameters.GetBool("Огонь По Своим");
+    room.Damage.GetContext().DamageOut.Value = gameModeParameters.GetBool("Damage");
+    room.BreackGraph.OnlyPlayerBlocksDmg = gameModeParameters.GetBool("PartialDesruction");
+    room.BreackGraph.WeakBlocks = gameModeParameters.GetBool("LoosenBlocks");
+}
+
+// задает настройки режима мир
+export function configure() {
+    room.Properties.GetContext().GameModeName.Value = "GameModes/Peace";// задаем название режима
+    room.Ui.GetContext().Hint.Value = "Добро пожаловать в Мытищи";// выводим подсказку
+    room.Ui.GetContext().QuadsCount.Value = true;// выводим количество квадов на карте
+    room.BreackGraph.BreackAll = true; // делаем так, чтобы можно было сломать любой блок
+    room.Spawns.GetContext().RespawnTime.Value = 5; //таймер респавна
+    set_build_settings();
+    set_inventory();
+    apply_room_options();
+}
+
+export function create_teams() {
+    // создаем команды
+    const roomParameters = room.GameMode.Parameters;
+    const hasRedTeam = roomParameters.GetBool("RedTeam");
+    const hasBlueTeam = roomParameters.GetBool("BlueTeam");
+    if (hasRedTeam || !hasRedTeam && !hasBlueTeam) {
+        teams.create_team_red();
+    }
+    if (hasBlueTeam || !hasRedTeam && !hasBlueTeam) {
+        const blueTeam = teams.create_team_blue();
+        if (roomParameters.GetBool("ГНИО")) {
+            set_empty_inventory(blueTeam.Inventory);
+        }
+    }
+
+    // по запросу на вход в команду - кидаем игрока в команду
+    room.Teams.OnRequestJoinTeam.Add(function (player, team) { team.Add(player); });
+    // если игрок сменил команду или выбрал ее, то происходит спавн игрока
+    room.Teams.OnPlayerChangeTeam.Add(function (player) { player.Spawns.Spawn() });
 }
