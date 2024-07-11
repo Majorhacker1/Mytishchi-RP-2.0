10f7cc79175b0b8b0c0f39ec4141e7c52adca2fe
Majorhacker1 <116291812+Majorhacker1@users.noreply.github.com>
2024-07-12 00:23:16 +0300

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -31,4 +31,30 @@ blue GameMode.Parameters.GetBool("BlueTeam");
 red GameMode.Parameters.GetBool("RedTeam")
 black GameMode.Parameters.GetBool("BlackTeam");
 green GameMode.Parameters.GetBool("GreenTeam");
+
 if (red || !red && ! blue)
+
+Teams.Add("Red", "МВД и Росгвардия", { г: 150 });
+
+Teams.Add("Black", "Рецидивисты", { р: 150});
+
+Teams.Add("Green", "Военнослужащие", { g: 150});
+
+if (blue || ! red && ! blue) {
+
+Teams.Add("Blue", "Граждане", { b: 150 });
+
+
+Teams.Get("Red").Spawns. SpawnPointsGroups.Add(2);
+
+Teams.Get("Blue").Spawns. SpawnPointsGroups.Add(1);
+
+if(GameMode.Parameters.GetBool ("ГНИО")) {
+
+
+var inventory Inventory.GetContext();
+
+
+Teams.Get("Blue"). Inventory.Main.Value = false;
+
+Teams.Get("Blue"). Inventory.Secondary.Value
